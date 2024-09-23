671ca7271d421269c354eed8a0b11d83bc9c152c
milanzivanov <milan.zivanov@gmail.com>
2024-09-23 14:54:30 +0200

    useRef for focus DOM input elements

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,34 @@
 /* eslint-disable react/prop-types */
+import { useEffect, useRef } from "react";
+
 function Search({ query, setQuery }) {
+  const inputElement = useRef(null);
+
+  // classic way
+  // useEffect(function () {
+  //   inputElement.current.focus();
+  // }, []);
+
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputElement.current) {
+          return;
+        }
+
+        if (e.code === "Enter") {
+          inputElement.current.focus();
+          setQuery("");
+        }
+      }
+
+      document.addEventListener("keydown", callback);
+
+      return () => document.removeEventListener("keydown", callback);
+    },
+    [setQuery]
+  );
+
   return (
     <input
       className="search"
@@ -7,6 +36,7 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputElement}
     />
   );
 }
