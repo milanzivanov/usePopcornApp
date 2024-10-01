bd4723df86b61aaf3b929d4a32d191c1a9f5bc56
milanzivanov <milan.zivanov@gmail.com>
2024-10-01 11:26:55 +0200

    testing with vercel

--- src/components/WatchedSummary.jsx
+++ src/components/WatchedSummary.jsx
@@ -1,11 +1,14 @@
 /* eslint-disable react/prop-types */
+
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 function WatchedSummary({ watched }) {
+  console.log(watched);
   const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
   const avgUserRating = average(watched.map((movie) => movie.userRating));
   const avgRuntime = average(watched.map((movie) => movie.runtime));
+
   return (
     <div className="summary">
       <h2>Movies you watched</h2>
@@ -24,7 +27,7 @@ function WatchedSummary({ watched }) {
         </p>
         <p>
           <span>⏳</span>
-          <span>{avgRuntime} min</span>
+          <span>{avgRuntime.toFixed(2)} min</span>
         </p>
       </div>
     </div>
